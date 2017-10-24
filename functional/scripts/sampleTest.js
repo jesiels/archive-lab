5bd6561232674944642936a6432dea933c6d7f07
Tarcisio Pereira <tpereira@br.ibm.com>
2017-10-24 13:03:36 +0000

    adding more visitors

--- functional/scripts/sampleTest.js
+++ functional/scripts/sampleTest.js
@@ -1,21 +1,35 @@
 module.exports = {
-  'Sample functional test' : function (browser) {
-    browser
-      .url('https://archive-lab-dev.mybluemix.net/')
-      .waitForElementPresent('#navbar-item-contact', 600000,true, function(){
-      	console.log('found Contact Button');
-      }, 'Waiting for Contact Button')
-      .click('#navbar-item-contact', function(){
-      	console.log('contact button Clicked');
-      })
-      .waitForElementVisible('#contact .btn', 600000, true,
-        function(){
-        	console.log("found it");
-        }, 'Waiting for Search Input to become visible')
-      .click('#contact .btn', function(){
-      	console.log('clicked');
-      })
-      .pause(5000)
-      .end();
+  	'Sample functional test' : function (browser) {
+		
+	    browser
+	      .url('https://archive-lab-dev.mybluemix.net/')
+	      .waitForElementPresent('#navbar-item-contact', 600000,true, function(){
+	      	console.log('found Contact Button');
+	      }, 'Waiting for Contact Button')
+	      .click('#navbar-item-contact', function(){
+	      	console.log('contact button Clicked');
+	      })
+	      .waitForElementVisible('#contact .btn', 600000, true,
+	        function(){
+	        	console.log("found it");
+	        }, 'Waiting for Search Input to become visible')
+	      .click('#contact .btn', function(){
+	      	console.log('clicked');
+	      });
+	    
+	      
+	      console.log(browser);
+	      
+	      var i = 0;
+	      while ( i < 1000 ) {
+	      		browser.click('#contact .btn' , function() {
+	      			console.log('contact btn click');
+	      		});  
+	      		i++;
+	      }
+  		browser
+ 		.pause(5000)
+	    .end();
+
   }
 };
