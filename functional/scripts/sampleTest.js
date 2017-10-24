ad79bbda98b6890e44a1c1236a16bc3ce1deeb43
Tarcisio Pereira <tpereira@br.ibm.com>
2017-10-24 12:55:00 +0000

    click contact button

--- functional/scripts/sampleTest.js
+++ functional/scripts/sampleTest.js
@@ -2,6 +2,12 @@ module.exports = {
   'Sample functional test' : function (browser) {
     browser
       .url('https://archive-lab-dev.mybluemix.net/')
+      .waitForElementPresent('#navbar-item-contact', 600000,true, function(){
+      	console.log('found Contact Button');
+      }, 'Waiting for Contact Button')
+      .click('#navbar-item-contact', function(){
+      	console.log('contact button Clicked');
+      })
       .waitForElementVisible('#contact .btn', 600000, true,
         function(){
         	console.log("found it");
