c6ed5a2d92293ba6bf0e4d21a5e7a56a0aa2e8a8
Tarcisio Pereira <tpereira@br.ibm.com>
2017-10-24 12:34:17 +0000

    testing land page

--- functional/scripts/sampleTest.js
+++ functional/scripts/sampleTest.js
@@ -1,11 +1,12 @@
 module.exports = {
   'Sample functional test' : function (browser) {
     browser
-      .url('http://google.com')
-      .waitForElementVisible('input.gsfi', 60000, true,
-        function(){}, 'Waiting for Search Input to become visible')
-      .setValue('input.gsfi', 'Test')
-      .waitForElementPresent('#search-page', 60000)
+      .url('https://archive-lab-dev.mybluemix.net/')
+      .waitForElementVisible('#contact .btn', 600000, true,
+        function(){
+        	console.log("found it");
+        }, 'Waiting for Search Input to become visible')
+      .click()
       .pause(5000)
       .end();
   }
